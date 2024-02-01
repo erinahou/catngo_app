2644cbf5a8c06bb8d1a44ff7b747f0f953ce8eae
rohan martin <rohan1215@hotmail.com>
2024-02-01 20:37:23 +1100

    finished create/new and assosiated view. User must be logged in to validate new cat

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,22 @@
 class CatsController < ApplicationController
+
+def new
+  @cat = Cat.new
+end
+
+def create
+    @cat = current_user.cats.build(cats_params)
+
+    if @cat.save
+      redirect_to root_path
+    else
+      render :new
+    end
+end
+
+private
+
+def cats_params
+  params.require(:cat).permit(:name, :description, :age, :price_per_day)
+end
 end
