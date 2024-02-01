f658e02419a031e61ac3f02315709bdd1148f088
rohan martin <rohan1215@hotmail.com>
2024-02-01 21:18:54 +1100

    finished edit/update for cats controller

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -21,10 +21,30 @@ def create
     end
 end
 
+def edit
+  @cat = Cat.find(params[:id])
+  redirect_to root_path unless can_edit_cat?(@cat)
+end
+
+def update
+  @cat = Cat.find(params[:id])
+
+  if can_edit_cat?(@cat)
+    @cat.update(cats_params)
+    redirect_to root_path
+  else
+    redirect_to root_path, alert: "This isnt your cat!"
+  end
+end
+
 private
 
 def cats_params
   params.require(:cat).permit(:name, :description, :age, :price_per_day)
+end
+
+def can_edit_cat?(cat)
+  cat.user == current_user
 end
 
 end
