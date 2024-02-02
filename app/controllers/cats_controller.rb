14f3265f0a70d6a3f6b4df8b0d86ae49e78212ab
Erina Ho <erina.hou@gmail.com>
2024-02-03 07:52:08 +1100

    Added indentation on cats controller

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,50 +1,50 @@
 class CatsController < ApplicationController
-def index
-  @cats = Cat.all
-end
+  def index
+    @cats = Cat.all
+  end
 
-def show
-  @cat = Cat.find(params[:id])
-end
+  def show
+    @cat = Cat.find(params[:id])
+  end
 
-def new
-  @cat = Cat.new
-end
+  def new
+    @cat = Cat.new
+  end
 
-def create
-    @cat = current_user.cats.build(cats_params)
+  def create
+      @cat = current_user.cats.build(cats_params)
 
-    if @cat.save
+      if @cat.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+    redirect_to root_path unless can_edit_cat?(@cat)
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+
+    if can_edit_cat?(@cat)
+      @cat.update(cats_params)
       redirect_to root_path
     else
-      render :new
+      redirect_to root_path, alert: "This isnt your cat!"
     end
-end
-
-def edit
-  @cat = Cat.find(params[:id])
-  redirect_to root_path unless can_edit_cat?(@cat)
-end
-
-def update
-  @cat = Cat.find(params[:id])
-
-  if can_edit_cat?(@cat)
-    @cat.update(cats_params)
-    redirect_to root_path
-  else
-    redirect_to root_path, alert: "This isnt your cat!"
   end
-end
 
-private
+  private
 
-def cats_params
-  params.require(:cat).permit(:name, :description, :age, :price_per_day)
-end
+  def cats_params
+    params.require(:cat).permit(:name, :description, :age, :price_per_day)
+  end
 
-def can_edit_cat?(cat)
-  cat.user == current_user
-end
+  def can_edit_cat?(cat)
+    cat.user == current_user
+  end
 
 end
